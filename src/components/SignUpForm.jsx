321fc150c014eb791b7557282fe3de11d961afb4
SeasonedSoup <emmetteagoncillo1@gmail.com>
2026-01-23 16:35:33 +0800

    Sign-up page is now working

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -4,27 +4,61 @@ function SignUpForm() {
     const [username, setUsername] = useState("")
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
+    const [confirmPassword, setConfirmPassword] = useState("")
 
-    async function signup() {
-        const url = "http://localhost:8000/signup"
+    async function signup(e) {
+        e.preventDefault();
 
+        const url = "http://localhost:8000/api/signup"
+
+        if (password !== confirmPassword) {
+            alert("passwords do not match");
+            return;
+        }
+        try {
+            const result = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+    
+            body: JSON.stringify({
+                username,
+                email,
+                password
+                }),
+            });
+
+            if(!result.ok) {
+                throw new Error(`Error status: ${result.status}`);
+            }
+            
+            const data = await result.json();
+            console.log('Success:', data);
+            alert('Sign-up successful!');
+
+        } catch(error) {
+            console.error(error);
+            alert("Signup failed check console.")
+        }
     }
     return (
         <>
             <h1>Sign-Up Form</h1>
 
-            <form action="#" method="POST">
+            <form method="POST" onSubmit={signup}>
                 <label htmlFor="username">Username: </label>
-                <input type="text" name="username" id="username" />
+                <input type="text" name="username" id="username" value={username} onChange={(e) => setUsername(e.target.value)}/>
 
-                <label htmlFor="email">Email </label>
-                <input type="email" name="email" id="email" />
+                <label htmlFor="email">Email:  </label>
+                <input type="email" name="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
 
                 <label htmlFor="password">Password: </label>
-                <input type="password" name="password" id="password" />
+                <input type="password" name="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
 
                 <label htmlFor="confirmPassword">Confirm Password: </label>
-                <input type="password" name="confirmPassword" id="confirmPassword" />
+                <input type="password" name="confirmPassword" id="confirmPassword" value={confirmPassword} 
+                onChange={(e) => setConfirmPassword(e.target.value)}/>
 
                 <button>Submit</button>
             </form>
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import LoginForm from './components/LoginForm'
+import SignUpForm from './components/SignUpForm'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <LoginForm/>
+    <SignUpForm />
   </StrictMode>,
 )
