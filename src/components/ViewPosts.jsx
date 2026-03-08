466762b64be559f1cd84ff45ef89853096b9e46c
SeasonedSoup <emmetteagoncillo1@gmail.com>
2026-03-08 22:33:12 +0800

    Add code that will refresh the posts after change
    not sure if api works yet not tested whether
    the following id will publish or unpublish
    when button is clicked will be tested soon

--- src/components/ViewPosts.jsx
+++ src/components/ViewPosts.jsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
 
 function ViewPosts() {
-    const [publishedPosts, setPublished] = useState([]);
-    const [unpublishedPosts, setUnpublished] = useState([])
+    const [publishedPosts, setPublishedPosts] = useState([]);
+    const [unpublishedPosts, setUnpublishedPosts] = useState([]);
     const [loading, setLoading] = useState(true)
 
     useEffect(() => {
@@ -20,31 +20,72 @@ function ViewPosts() {
             const unpublished = posts.filter((post) => post.published === false)
             console.log(published)
             console.log(unpublished)
-            setPublished(published);
-            setUnpublished(unpublished);
+            setPublishedPosts(published);
+            setUnpublishedPosts(unpublished);
             setLoading(false)
         }
 
         fetchPosts();
     }, []);
+
+    async function togglePublishStatus(id, publishStatus) {
+        if (publishStatus === true) {
+            const post = publishedPosts.find(p => p.id === id)
+            setPublishedPosts(prev => prev.filter(p => p.id !== id))
+            setUnpublishedPosts(prev => [...prev, {...post, published: false}])
+        }  else {
+            const post = publishedPosts.find(p => p.id === id)
+            setUnpublishedPosts(prev => prev.filter(p => p.id !== id))
+            setPublishedPosts(prev => [...prev, {...post, published: true}])
+        }
+
+
+        const url = "http://localhost:8000/api/togglepubstatus"
+
+        const response = await fetch(url, {
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                publishStatus
+            })
+    
+        })
+        
+        const result = response.json();
+        console.log(result)
+    }
   
     if (loading !== true) {
         return (
-            <div class="container">
-                <div class="card">
+            <div className="container">
+                <div className="card">
                     <h1>Published Posts</h1>
-                     {publishedPosts.length > 0 ? (
-                        <h1>Published Posts Found</h1>
+                    {publishedPosts.length > 0 ? (
+                        publishedPosts.map(publishedPost => {
+                        return (
+                        <div key={publishedPost.id} >
+                        <h1> {publishedPost.title}</h1>
+                        <button onClick={() => togglePublishStatus(publishedPost.id, true)}>Unpublish</button>
+                        </div>
+                        )
+                    })
                     ) : <h1>No Published Posts Found</h1>
                     } 
                 </div>
 
-                <div class="card">
+                <div className="card">
                     <h1>Unpublished Posts</h1>
                     {unpublishedPosts.length > 0 ? (
-                        <h1>Unpublished Posts Found</h1>
+                        unpublishedPosts.map(unpublishedPost => {
+                            return (
+                            <div  key={unpublishedPost.id} >
+                            <h1> {unpublishedPost.title}</h1>
+                            <button onClick={() => togglePublishStatus(unpublishedPost.id, false)}>Publish</button>
+                            </div>
+                            )
+                        })
                     ) : <h1>No Unpublished Posts Found</h1>
-                    
                     } 
                 </div>
             </div>
